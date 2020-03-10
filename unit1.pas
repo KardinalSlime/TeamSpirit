caee3503ad60f4ba7901a2ce951c38ba0bfc9a1c
Vladimir5349 <vovan51rus5349@gmail.com>
2020-03-10 11:52:49 +0300

    NEW

--- unit1.pas
+++ unit1.pas
@@ -12,18 +12,27 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    File: TMainMenu;
     Fille: TMainMenu;
-    Edit: TMainMenu;
-    Cut: TMenuItem;
-    MenuItem1: TMenuItem;
-    VblDLIT: TMenuItem;
-    Reference: TMainMenu;
-    Search: TMainMenu;
-    View: TMainMenu;
     Memo1: TMemo;
     Create: TMenuItem;
     Close: TMenuItem;
-    Log_out: TMenuItem;
+    MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
+    MenuItem11: TMenuItem;
+    MenuItem12: TMenuItem;
+    MenuItem13: TMenuItem;
+    MenuItem14: TMenuItem;
+    MenuItem15: TMenuItem;
+    MenuItem16: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
     Save_as: TMenuItem;
     Save: TMenuItem;
     Open: TMenuItem;
